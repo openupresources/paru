a9808814519268a8cd648b6e912a8a1aa180d015
Dan Blaker <danblaker@gmail.com>
2019-08-29 11:30:42 -0700

    allow underscores in class selectors

--- lib/paru/selector.rb
+++ lib/paru/selector.rb
@@ -51,16 +51,16 @@ module Paru
         # @return [Boolean] True if the node in the context of the
         #   filtered_nodes is selected by this Selector
         def matches? node, filtered_nodes
-            node.type == @type and 
-                @classes.all? {|c| node.has_class? c } and    
+            node.type == @type and
+                @classes.all? {|c| node.has_class? c } and
                 @relations.all? {|r| r.matches? node, filtered_nodes}
         end
 
-        private 
+        private
 
         S = /\s*/
-        TYPE = /(?<type>(?<name>[A-Z][a-zA-Z]*)(?<classes>(\.[a-zA-Z-]+)*))/
-        OTHER_TYPE = /(?<other_type>(?<other_name>[A-Z][a-zA-Z]*)(?<other_classes>(\.[a-zA-Z-]+)*))/
+        TYPE = /(?<type>(?<name>[A-Z][a-zA-Z]*)(?<classes>(\.[_a-zA-Z-]+)*))/
+        OTHER_TYPE = /(?<other_type>(?<other_name>[A-Z][a-zA-Z]*)(?<other_classes>(\.[_a-zA-Z-]+)*))/
         OPERATOR = /(?<operator>\+|-|>)/
         DISTANCE = /(?<distance>[1-9][0-9]*)/
         RELATION = /(?<relation>#{S}#{OTHER_TYPE}#{S}#{OPERATOR}#{S}#{DISTANCE}?#{S})/
@@ -74,14 +74,14 @@ module Paru
 
             while continue_parsing? partial_match
                 operator = expect partial_match, :operator
-                distance = expect_integer partial_match, :distance 
+                distance = expect_integer partial_match, :distance
                 type, classes = expect_pandoc_other_type partial_match
 
                 @relations.push Relation.new(operator, distance, type, classes)
 
                 partial_match = rest partial_match
             end
-        end 
+        end
 
         # Is type actually a Pandoc AST node type?
         def is_pandoc_type(type)
@@ -143,8 +143,8 @@ module Paru
         end
 
         def matches?(node, filtered_nodes)
-            level_nodes = filtered_nodes.keep_if do |n| 
-                node.is_inline? == n.is_inline? or 
+            level_nodes = filtered_nodes.keep_if do |n|
+                node.is_inline? == n.is_inline? or
                     node.can_act_as_both_block_and_inline?
             end
             previous_nodes = previous level_nodes, @distance
@@ -157,7 +157,7 @@ module Paru
                 is_descendant? node
             else
                 false
-            end 
+            end
         end
 
         def in_sequence?(node, previous_nodes)
